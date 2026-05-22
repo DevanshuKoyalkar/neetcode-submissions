20c847ccba6c70a5e0bb6ab0df18923df22028c7
DevanshuKoyalkar <devanshukoyalkar1@gmail.com>
2026-05-22 18:19:41 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        result = 0
+        select, dont_select = 0, 0
+
+        for i in range(n):
+            select, dont_select = dont_select+nums[i], max(select,dont_select)
+            result = max(select, dont_select)
+        return result
+
+        
