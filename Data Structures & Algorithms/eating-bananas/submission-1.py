65dd3f43ccfa8e6bfa771783b67ee055b70ce1b2
DevanshuKoyalkar <devanshukoyalkar1@gmail.com>
2026-05-25 20:37:20 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,30 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        sum_piles = sum(piles)
+
+        MAX = 1_000_000_000
+        MIN = 1
+
+
+        # F F F F T T 
+        # invariant --> number of hours taken to eat is > H
+        # first T such that we can each 
+
+        start, end = MIN, MAX
+        result = -1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            num_hours = 0
+            for pile in piles:
+                num_hours += math.ceil(float(pile) / mid)
+
+            if num_hours > h:
+                start = mid + 1
+            else:
+                result = mid
+                end = mid - 1
+        
+        return result
