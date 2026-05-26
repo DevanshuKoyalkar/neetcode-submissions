600363aed3ccfa1464668fa944614a99a785f8a3
DevanshuKoyalkar <devanshukoyalkar1@gmail.com>
2026-05-25 20:48:43 -0400

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    digit_to_letter = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
+        }
+    
+    def letterCombinations(self, digits: str) -> List[str]:
+        LEN = len(digits)
+        result = []
+
+        if LEN == 0: 
+            return result
+        
+        if LEN == 1:
+            return [ch for ch in self.digit_to_letter[digits]]
+
+        tail_result = self.letterCombinations(digits[1:]) 
+        
+        for cand in tail_result:
+            # print(digits[0], self.digit_to_letter[digits[0]])
+            for char in self.digit_to_letter[digits[0]]:
+                
+                result.append(char + cand)
+        
+        return result
+
