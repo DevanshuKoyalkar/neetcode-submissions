3de63d300b503b60845f1da3f251dcc08af7b594
DevanshuKoyalkar <devanshukoyalkar1@gmail.com>
2026-05-28 22:44:22 -0400

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        ROWS, COLS = len(word1), len(word2)
+
+        dp = [float("inf")] * (COLS+1)
+
+        for j in range(COLS + 1):
+            dp[j] = j
+
+        for i in range(1, ROWS + 1):
+            next_dp = [float("inf")] * (COLS+1)
+            next_dp[0] = i
+            for j in range(1, COLS + 1):
+                next_dp[j] = 1 + min([next_dp[j-1], dp[j], dp[j-1]])
+                if word1[i - 1] == word2[j - 1]:
+                    next_dp[j] = min(next_dp[j], dp[j-1])
+
+            dp = next_dp
+        
+
+        return dp[COLS]
+
+    def minDistance1(self, word1: str, word2: str) -> int:
+
+        ROWS, COLS = len(word1), len(word2)
+
+        min_dist = [[float("inf")] * (COLS+1) for _ in range(ROWS + 1)]
+
+        for j in range(COLS + 1):
+            min_dist[0][j] = j
+
+        for i in range(ROWS + 1):
+            min_dist[i][0] = i
+        
+        for i in range(1, ROWS + 1):
+            for j in range(1, COLS + 1):
+                min_dist[i][j] = 1 + min([min_dist[i][j-1], min_dist[i-1][j], min_dist[i-1][j-1]])
+                if word1[i - 1] == word2[j - 1]:
+                    min_dist[i][j] = min(min_dist[i][j], min_dist[i-1][j-1])
+        
+        # for row in min_dist:
+        #     print(row)
+        return min_dist[ROWS][COLS]
